fb6071e3cf4341026df6603c1e609caa868de87a
VGan <vedantgannu@gmail.com>
2023-12-12 05:51:49 -0500

    small changes

---
+++ bert/highlighted_sentence_data.py
@@ -0,0 +1,45 @@
+# Change directory into the bert_data_cnndm_final directory
+# cd bert_data_cnndm_final
+# python ../highlighted_sentence_data.py ../highlighted_sentence_data_dir train
+
+
+import torch
+import sys
+
+OUTPUT_DIR=sys.argv[1]
+mode=sys.argv[2]
+
+
+if mode == 'train':
+    total_num = 144
+elif mode == 'valid':
+    total_num = 7
+elif mode == 'test':
+    total_num = 6
+
+
+cnt = 0
+for i in range(total_num):
+    a=torch.load(f'cnndm.{mode}.{i}.bert.pt')
+    out_pt=f'{OUTPUT_DIR}/cnndm.{mode}.{i}.bert.pt'
+    out_l = []
+    for j in a:
+        keys = ['src', 'tgt', 'src_sent_labels', 'segs', 'clss', 'z', 'src_txt', 'tgt_txt']
+        it = {}
+        for k in keys:
+            if k == 'z':
+                src = j['src']
+                clss = j['clss']
+                z = []
+                for sent_id, v in enumerate(j['src_sent_labels']):
+                    if v == 1:
+                        sent_id = int(float(sent_id))
+                        start = clss[sent_id]
+                        end = clss[sent_id+1] if not sent_id == len(clss) - 1 else None
+                        tokens = src[start:end] if not end is None else src[start:]
+                        z.extend(tokens)
+                it[k] = z
+            else:
+                it[k] = j[k]
+        out_l.append(it)
+    torch.save(out_l, out_pt)
--- bert/scribble_files/read_from_tensor.py
+++ bert/scribble_files/read_from_tensor.py
@@ -1,4 +1,6 @@
 import torch
-with open('stuff_inside_tensor.txt', 'w') as f:
-    for line in torch.load("./bert_data_cnndm_final/cnndm.train.0.bert.pt"):
+with open('stuff_inside_tensor_test_oracle.txt', 'w') as f:
+    # for line in torch.load("../bert_data_cnndm_final/cnndm.test.0.bert.pt"):
+    # for line in torch.load("../highlighted_sentence_data_dir/cnndm.train.0.bert.pt"):
+    for line in torch.load("../highlighted_sentence_data_dir_20k/oracle_test/cnndm.test.0.bert.pt"):
         f.write(f"{line}\n")
